377a314470d6d9f5a9525fc5ba919510804e5eea
Justin Wheeler <justinwwheeler@gmail.com>
2021-09-14 22:37:33 +0000

    add two functions to client.js, one for createTweetElements and one to renderTweets from a database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,69 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+$(document).ready(function() {
+  
+  /**
+   * createTweetElement
+   * @param { object } tweet 
+   * @returns { html element } < article >
+   */
+  const createTweetElement = function(tweet) {
+  return $(`
+  <article class="tweet">
+    <header>
+      <div>
+        <img src="${tweet.user.avatars}">
+        <span>${tweet.user.name}</span>
+      </div>
+      <span class="handle">${tweet.user.handle}</span>
+    </header>
+    <p>${tweet.content.text}</p>
+    <footer>
+      <time>${tweet.created_at}</time>
+      <div>
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+  </article>
+  `);
+  };
+  
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $newTweet = createTweetElement(tweet);
+      $('#tweets-container').append($newTweet); 
+    }
+  }
+
+  renderTweets(data);
+
+});
+
