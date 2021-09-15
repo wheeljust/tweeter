3378b342f39557bee287605104600296318fb14e
Justin Wheeler <justinwwheeler@gmail.com>
2021-09-15 21:58:56 +0000

    add filters for new-tweet submit listener to prevent posts with no text and post that exceed char limit of 140

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,59 +4,34 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
 $(document).ready(function() {
   
   /**
    * createTweetElement
-   * @param { object } tweet 
+   * @param { object } tweet
    * @returns { html element } < article >
    */
   const createTweetElement = function(tweet) {
-  return $(`
-  <article class="tweet">
-    <header>
-      <div>
-        <img src="${tweet.user.avatars}">
-        <span>${tweet.user.name}</span>
-      </div>
-      <span class="handle">${tweet.user.handle}</span>
-    </header>
-    <p>${tweet.content.text}</p>
-    <footer>
-      <time>${tweet.created_at}</time>
-      <div>
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-      </div>
-    </footer>
-  </article>
-  `);
+    return $(`
+    <article class="tweet">
+      <header>
+        <div>
+          <img src="${tweet.user.avatars}">
+          <span>${tweet.user.name}</span>
+        </div>
+        <span class="handle">${tweet.user.handle}</span>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer>
+        <time>${tweet.created_at}</time>
+        <div>
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+    `);
   };
   
   /**
@@ -67,10 +42,14 @@ $(document).ready(function() {
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
       const $newTweet = createTweetElement(tweet);
-      $('#tweets-container').append($newTweet); 
+      $('#tweets-container').append($newTweet);
     }
-  }
+  };
 
+  /**
+   * loadTweets
+   * Perfoms an AJAX get request to /tweets and then calls renderTweets function
+   */
   const loadTweets = function() {
     $.ajax({
       method: "GET",
@@ -80,28 +59,28 @@ $(document).ready(function() {
         console.log(tweets); // remove later
         renderTweets(tweets);
       }
-    })
+    });
   };
 
   loadTweets();
 
-  /** Event listener for New Tweet Submit
-   *  Completes an AJAX post request when triggered
+  /** Event listener for SUBMIT a form with id="new-tweet"
+   * Filters and alerts submit events with no text, or events exceeding character limits
+   * Completes an AJAX post request when tweetBody meets the criteria to post
    */
-  const $form = $("#new-tweet");
-  $form.on("submit", function(event) {
+  $("#new-tweet").on("submit", function(event) {
     event.preventDefault();
     const $tweetBody = $(this).serialize();
+    const $charCount = 140 - $(".counter").val();
+
+    if (!$charCount) return alert("Opps! Please enter some text for your tweet before posting");
+    if ($charCount > 140) return alert("Opps! Your tweet exceeds the character limit!");
     
     $.post('/tweets', $tweetBody)
-      .then((res) => {
-        console.log('ajax post is working');  // do I need this .then()? also where is the $tweetBody data going
-        // loadTweets();
+      .then(() => {
+        console.log('ajax post is working');
+        this.reset();
       });
-
   });
-
-  // renderTweets(data);
-
 });
 
