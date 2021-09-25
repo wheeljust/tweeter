b38c006457070e47f4927ca669b958fd2fff8edf
Justin Wheeler <justinwwheeler@gmail.com>
2021-09-25 20:54:33 +0000

    refactor some style and comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,13 +4,13 @@
  */
 
 $(document).ready(function() {
-  
+
   /**
    *
    * @param { string } str user entered text string
    * @returns safe text string by escaping unsafe chars - prevents XSS (cross-site scripting)
    */
-  const escape = function(str) {
+   const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
